7a06d0ddb891d27612e35eeaeb6cc5e5c26c4b96
TetewHeroez <teosofihidayahagung@gmail.com>
2024-09-02 21:46:24 +0700

    +

---
+++ Semester 3/Metode Numerik/Akar Persamaan/RegulaFalsi.py
@@ -0,0 +1,26 @@
+def f(x):
+    return x**4 - 2*x**2 + x - 2
+def regula_false(a, b):
+    err = 10**(-3)
+    if f(a) * f(b) > 0:
+        print("Nilai a dan b tidak sesuai")
+        return None
+    elif f(a) == 0:
+        return a
+    elif f(b) == 0:
+        return b
+    xr = b-(f(b)*(b-a))/(f(b)-f(a))
+    i=0
+    while (abs(f(xr)) > err) & (i < 100):
+        if f(a) * f(xr) < 0:
+            b = xr
+        else:
+            a = xr
+        xr = b-(f(b)*(b-a))/(f(b)-f(a))
+        i += 1
+    print("Iterasi: ", i)
+    return xr
+
+a = float(input("Masukkan nilai a: "))
+b = float(input("Masukkan nilai b: "))
+print("Akar persamaan: ", regula_false(a, b))
