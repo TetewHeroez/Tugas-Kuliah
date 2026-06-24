237b0fff34ab3b34e30ae741ecaa35d67412f3ad
Teosofi Agung <teosofihidayahagung@gmail.com>
2026-06-24 20:26:15 +0700

    .

--- Semester 8/Tugas Akhir/Sidang TA/test.py
+++ Semester 8/Tugas Akhir/Sidang TA/test.py
@@ -8,14 +8,13 @@ class RevisionScene(Scene):
         centralizer(self)
 
 def centralizer(s):
-    # 1. Judul & Definisi 
     judul = Title("Centralizer", color=BLACK, font_size=48, include_underline=True)
     judul[-1].set_color(BLACK)
     
     teks_atas = Tex(
     r"\parbox{"
     + str(config.frame_width)
-    + r"cm}{\textbf{Definisi 3}. Centralizer dari suatu permutasi $\sigma \in S_n$ didefinisikan sebagai himpunan semua permutasi $\tau \in S_n$ yang komutatif dengan $\sigma$. Secara ekuivalen, centralizer memuat seluruh permutasi yang mempertahankan struktur sikel dari $\sigma$.}",
+    + r"cm}{\textbf{Definisi 3}. Centralizer dari suatu permutasi $\sigma \in S_n$ didefinisikan sebagai himpunan semua permutasi $\tau \in S_n$ yang komutatif dengan $\sigma$.}",
     color=BLACK,
     font_size=30).next_to(judul, DOWN, buff=0.5)
 
@@ -27,173 +26,106 @@ def centralizer(s):
     teks_bawah = Tex(
     r"\parbox{"
     + str(config.frame_width)
-    + r"cm}{Misalkan $\sigma\in S_n$ memiliki $a_k$ sikel berpanjang $k$ untuk setiap $k=1,2,\ldots,n$. Maka}",
+    + r"cm}{\textbf{Teorema 3}. Misalkan $\sigma\in S_n$ memiliki $a_k$ sikel berpanjang $k$ untuk setiap $k=1,2,\ldots,n$. Maka}",
     color=BLACK,
-    font_size=30
-).next_to(definisi, DOWN, buff=0.6)
+    font_size=30).next_to(definisi, DOWN, buff=0.5)
 
     teorema = MathTex(
         r"C_{S_n}(\sigma) \cong \prod_{k=1}^{n} \left( \mathbb{Z}_k^{a_k} \rtimes S_{a_k} \right)",
         color=BLACK, font_size=30
-    ).next_to(teks_bawah, DOWN, buff=0.8)
+    ).next_to(teks_bawah, DOWN, buff=0.5)
 
     s.play(Write(judul), run_time=1)
-    s.play(Write(teks_atas,definisi), run_time=1.5)
-    s.play(Write(teks_bawah,teorema), run_time=1)
+    s.play(Write(teks_atas), Write(definisi), run_time=1)
+    s.play(Write(teks_bawah), Write(teorema), run_time=1)
     s.wait(2)
 
-    # 2. Transisi ke Contoh (Morphing, tanpa langkah-langkah kaku)
-    teks_contoh = Tex(r"\parbox{" + str(config.frame_width) + r"cm}{Tinjau permutasi $\sigma \in S_9$ berikut:}", color=BLACK, font_size=30).move_to(teks_atas)
-
+    # 2. Kemunculan Sigma dan Substitusi Teorema Barengan
     sigma_tex = MathTex(
-        r"\sigma =", r"(1\;2\;3)", r"(4\;5\;6)", r"(7\;8)", r"(9)",
-        color=BLACK, font_size=30
-    ).next_to(teks_contoh, DOWN, buff=0.5)
+        r"\sigma =", r"(1\;3\;4)", r"(2\;7\;8)", r"(5\;6\;9)", r"\in S_9",
+        color=BLACK, font_size=48
+    ).next_to(judul, DOWN, buff=0.5)
     
-    sigma_tex[1].set_color(BLUE_E)     # (1 2 3)
-    sigma_tex[2].set_color(RED_E)      # (4 5 6)
-    sigma_tex[3].set_color(GREEN_E)    # (7 8)
-    sigma_tex[4].set_color(ORANGE)     # (9)
+    sigma_tex[1].set_color(BLUE_E)     # (1 3 4)
+    sigma_tex[2].set_color(RED_E)      # (2 7 8)
+    sigma_tex[3].set_color(GREEN_E)    # (5 6 9)
+
+    subst_final = MathTex(
+        r"C_{S_9}(\sigma) \cong \mathbb{Z}_3^3 \rtimes S_3",
+        color=BLACK, font_size=36
+    ).next_to(sigma_tex, DOWN, buff=0.8)
 
     s.play(
-        Transform(teks_atas, teks_contoh),
-        teorema.animate.next_to(sigma_tex, DOWN, buff=0.8),
+        FadeOut(VGroup(teks_atas, definisi, teks_bawah)),
+        Write(sigma_tex),
+        Transform(teorema, subst_final),
+        run_time=2
+    )
+    s.wait(2)
+
+    # 4. Pisahkan C_S_9 dan Z_3^3 x S_3 dengan warna
+    grup_kiri = MathTex(r"C_{S_9}(\sigma)", color=BLUE_E, font_size=40).move_to(LEFT * 3.5 + UP * 1.0)
+    grup_kanan = MathTex(r"\mathbb{Z}_3^3 \rtimes S_3", color=RED_E, font_size=40).move_to(RIGHT * 3.5 + UP * 1.0)
+
+    s.play(
+        FadeOut(teorema), 
+        FadeIn(grup_kiri, shift=LEFT),
+        FadeIn(grup_kanan, shift=RIGHT),
         run_time=1.5
     )
-    s.play(Write(sigma_tex), run_time=1)
+
+    # Panah melengkung
+    panah_atas = CurvedArrow(grup_kiri.get_top() + RIGHT*0.2 + UP*0.2, grup_kanan.get_top() + LEFT*0.2 + UP*0.2, angle=-TAU/8, color=BLACK)
+    panah_bawah = CurvedArrow(grup_kanan.get_bottom() + LEFT*0.2 + DOWN*0.2, grup_kiri.get_bottom() + RIGHT*0.2 + DOWN*0.2, angle=-TAU/8, color=BLACK)
+
+    label_phi = MathTex(r"\Phi", color=BLACK, font_size=32).next_to(panah_atas, UP, buff=0.1)
+    label_phi_inv = MathTex(r"\Phi^{-1}", color=BLACK, font_size=32).next_to(panah_bawah, DOWN, buff=0.1)
+
+    s.play(Create(panah_atas), Write(label_phi), Create(panah_bawah), Write(label_phi_inv))
     s.wait(1)
 
-    # 3. Substitusi Teorema Langsung (Visual Morphing)
-    subst_final = MathTex(
-        r"C_{S_9}(\sigma) \cong (\mathbb{Z}_3^2 \rtimes S_2) \times \mathbb{Z}_2",
-        color=BLACK, font_size=30
-    ).move_to(teorema)
+    # 5. Animasi Pemetaan Elemen (Total 7 Elemen) dijejerin ke bawah
+    kiri_group = VGroup()
+    kanan_group = VGroup()
 
-    s.play(Transform(teorema, subst_final), run_time=1.5)
-    s.wait(2)
+    examples = [
+        (r"id", r"((0,0,0), id)"),
+        (r"(1\;3\;4)", r"((1,0,0), id)"),
+        (r"(2\;8\;7)", r"((0,2,0), id)"),
+        (r"(1\;3\;4)(5\;6\;9)", r"((1,0,1), id)"),
+        (r"(1\;2)(3\;7)(4\;8)", r"((0,0,0), (1\;2))"),
+        (r"(1\;5\;3\;6\;4\;9)(2\;8\;7)", r"((1,2,0), (1\;3))"),
+        (r"(1\;4\;3)(2\;9\;7\;5\;8\;6)", r"((2,2,2), (2\;3))")
+    ]
 
-    # Bersihkan teks atas untuk fokus ke analisis
-    s.play(
-        FadeOut(teks_atas),
-        sigma_tex.animate.next_to(judul, DOWN, buff=0.3),
-        teorema.animate.next_to(judul, DOWN, buff=1.3),
-        run_time=1.5
-    )
+    kiri_y_start = grup_kiri.get_bottom()[1] - 0.5
+    for i, (eks_kiri, eks_kanan) in enumerate(examples):
+        center_kiri = MathTex(eks_kiri, color=BLUE_E, font_size=36).move_to(LEFT*2 + DOWN*1.0)
+        panah_tengah = MathTex(r"\longleftrightarrow", color=BLACK, font_size=36).move_to(DOWN*1.0)
+        center_kanan = MathTex(eks_kanan, color=RED_E, font_size=36).move_to(RIGHT*2 + DOWN*1.0)
+        
+        s.play(FadeIn(center_kiri, shift=UP), FadeIn(panah_tengah), FadeIn(center_kanan, shift=UP), run_time=0.6)
+        s.wait(0.5)
 
-    # 4. Peta Isomorfisma
-    teks_peta = Tex(r"\textbf{Peta Isomorfisma ($\Phi$)}", color=BLACK, font_size=32).next_to(teorema, DOWN, buff=0.6)
-    
-    phi_math = MathTex(
-        r"\Phi: C_{S_9}(\sigma) \longrightarrow (\mathbb{Z}_3^2 \rtimes S_2) \times \mathbb{Z}_2",
-        color=BLACK, font_size=36
-    ).next_to(teks_peta, DOWN, buff=0.3)
-    
-    phi_def = MathTex(
-        r"\Phi(\tau) = ((a,b), \pi_\tau, \varepsilon)",
-        color=BLACK, font_size=36
-    ).next_to(phi_math, DOWN, buff=0.3)
+        target_kiri = MathTex(eks_kiri, color=BLUE_E, font_size=28).move_to(grup_kiri.get_x() * RIGHT + (kiri_y_start - i*0.45) * UP)
+        target_kanan = MathTex(eks_kanan, color=RED_E, font_size=28).move_to(grup_kanan.get_x() * RIGHT + (kiri_y_start - i*0.45) * UP)
+        
+        s.play(
+            FadeOut(panah_tengah),
+            Transform(center_kiri, target_kiri),
+            Transform(center_kanan, target_kanan),
+            run_time=0.8
+        )
+        kiri_group.add(center_kiri)
+        kanan_group.add(center_kanan)
+        s.wait(0.3)
 
-    s.play(Write(teks_peta))
-    s.play(Write(phi_math))
-    s.play(Write(phi_def))
-    s.wait(2)
+    # 6. Kotakin kedua himpunan
+    kotak_kiri = SurroundingRectangle(VGroup(grup_kiri, kiri_group), color=BLUE_E, buff=0.3)
+    kotak_kanan = SurroundingRectangle(VGroup(grup_kanan, kanan_group), color=RED_E, buff=0.3)
 
-    # Pindahkan phi_def ke atas (menyederhanakan layar)
-    s.play(
-        FadeOut(teks_peta), FadeOut(phi_math),
-        phi_def.animate.next_to(teorema, DOWN, buff=0.5)
-    )
+    s.play(Create(kotak_kiri), Create(kotak_kanan), run_time=1.5)
+    s.wait(3)
 
-    # --- 5. Analisis per Siklus ---
-    analisa_teks = Tex(r"\textbf{Analisis per Siklus}", color=BLACK, font_size=32).next_to(phi_def, DOWN, buff=0.5)
-    s.play(Write(analisa_teks))
-
-    # Parameter a,b (3-cycle rotasi)
-    s.play(
-        sigma_tex[3].animate.set_color(GRAY).set_opacity(0.3),
-        sigma_tex[4].animate.set_color(GRAY).set_opacity(0.3),
-        run_time=1
-    )
-
-    ket_ab = Tex(r"\parbox{" + str(config.frame_width-1) + r"cm}{\textbf{$a, b \in \mathbb{Z}_3$}\\ Mewakili jumlah rotasi pada dua siklus 3. \\ Contoh: rotasi pada siklus pertama ($(1\;2\;3)^2 = (1\;3\;2)$) memiliki nilai $a=2$.}", color=BLACK, font_size=28).next_to(analisa_teks, DOWN, buff=0.4)
-    s.play(Write(ket_ab))
-
-    c1_copy = sigma_tex[1].copy()
-    c2_copy = sigma_tex[2].copy()
-    anim_z3 = VGroup(c1_copy, c2_copy).arrange(RIGHT, buff=0.5).next_to(ket_ab, DOWN, buff=0.5)
-    s.play(TransformFromCopy(sigma_tex[1], c1_copy), TransformFromCopy(sigma_tex[2], c2_copy))
-    
-    p1, p2, p3 = c1_copy[1].get_center(), c1_copy[2].get_center(), c1_copy[3].get_center()
-    s.play(
-        c1_copy[1].animate.move_to(p2),
-        c1_copy[2].animate.move_to(p3),
-        c1_copy[3].animate.move_to(p1),
-        path_arc=PI/2, run_time=1.5
-    )
-    s.wait(2)
-
-    # Parameter \pi_\tau (3-cycle swap)
-    ket_pi = Tex(r"\parbox{" + str(config.frame_width-1) + r"cm}{\textbf{$\pi_\tau \in S_2$}\\ Menentukan apakah kedua siklus 3 ditukar posisinya secara utuh.}", color=BLACK, font_size=28).move_to(ket_ab)
-    
-    s.play(FadeOut(ket_ab), FadeIn(ket_pi))
-    
-    box_b = SurroundingRectangle(c1_copy, color=BLUE_E, buff=0.1)
-    box_r = SurroundingRectangle(c2_copy, color=RED_E, buff=0.1)
-    s.play(Create(box_b), Create(box_r))
-    g_b = VGroup(c1_copy, box_b)
-    g_r = VGroup(c2_copy, box_r)
-    pos_b, pos_r = g_b.get_center(), g_r.get_center()
-    
-    s.play(
-        g_b.animate.move_to(pos_r),
-        g_r.animate.move_to(pos_b),
-        path_arc=PI/3, run_time=1.5
-    )
-    s.play(FadeOut(box_b), FadeOut(box_r))
-    s.wait(2)
-
-    # Parameter \varepsilon (2-cycle)
-    s.play(FadeOut(ket_pi), FadeOut(c1_copy), FadeOut(c2_copy))
-
-    s.play(
-        sigma_tex[1].animate.set_color(GRAY).set_opacity(0.3),
-        sigma_tex[2].animate.set_color(GRAY).set_opacity(0.3),
-        sigma_tex[3].animate.set_color(GREEN_E).set_opacity(1),
-        run_time=1
-    )
-
-    ket_eps = Tex(r"\parbox{" + str(config.frame_width-1) + r"cm}{\textbf{$\varepsilon \in \mathbb{Z}_2$}\\ Menentukan rotasi pada siklus $(7\;8)$. \\ Nilainya berupa identitas ($e$) atau berotasi menjadi $(8\;7)$.}", color=BLACK, font_size=28).move_to(ket_ab)
-    s.play(Write(ket_eps))
-
-    c3_copy = sigma_tex[3].copy().next_to(ket_eps, DOWN, buff=0.5)
-    s.play(TransformFromCopy(sigma_tex[3], c3_copy))
-
-    p7, p8 = c3_copy[1].get_center(), c3_copy[2].get_center()
-    s.play(
-        c3_copy[1].animate.move_to(p8),
-        c3_copy[2].animate.move_to(p7),
-        path_arc=PI/2, run_time=1.5
-    )
-    s.wait(2)
-
-    s.play(FadeOut(ket_eps), FadeOut(c3_copy), FadeOut(analisa_teks))
-
-    # Reset opacity
-    s.play(
-        sigma_tex[1].animate.set_color(BLUE_E).set_opacity(1),
-        sigma_tex[2].animate.set_color(RED_E).set_opacity(1),
-        sigma_tex[3].animate.set_color(GREEN_E).set_opacity(1),
-        sigma_tex[4].animate.set_color(ORANGE).set_opacity(1),
-        run_time=1
-    )
-    
-    # 6. Kesimpulan elemen
-    judul_elemen = Tex(r"\textbf{Kesimpulan}", color=BLACK, font_size=32).next_to(phi_def, DOWN, buff=0.5)
-    s.play(Write(judul_elemen))
-
-    kesimpulan = Tex(r"\parbox{" + str(config.frame_width-1) + r"cm}{Ketiga parameter $(a,b), \pi_\tau, \varepsilon$ divariasikan secara bebas untuk membentuk seluruh elemen di $C_{S_9}(\sigma)$.\\ \vspace{0.2cm} $|C_{S_9}(\sigma)| = (3^2 \cdot 2!) \cdot 2 = 36$ elemen.}", color=BLACK, font_size=30).next_to(judul_elemen, DOWN, buff=0.4)
-    
-    s.play(Write(kesimpulan))
-    s.wait(4)
-
-    s.play(FadeOut(VGroup(judul, teorema, sigma_tex, phi_def, judul_elemen, kesimpulan)))
+    # Fade Out Semua
+    s.play(FadeOut(VGroup(judul, sigma_tex, grup_kiri, grup_kanan, panah_atas, panah_bawah, label_phi, label_phi_inv, kiri_group, kanan_group, kotak_kiri, kotak_kanan)))
