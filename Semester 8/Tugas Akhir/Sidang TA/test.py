8ac9357b11e4256668597478eea136f8f66fb806
Teosofi Agung <teosofihidayahagung@gmail.com>
2026-06-25 14:32:56 +0700

    .

--- Semester 8/Tugas Akhir/Sidang TA/test.py
+++ Semester 8/Tugas Akhir/Sidang TA/test.py
@@ -43,16 +43,16 @@ def centralizer(s):
     # 2. Kemunculan Sigma dan Substitusi Teorema Barengan
     sigma_tex = MathTex(
         r"\sigma =", r"(1\;3\;4)", r"(2\;7\;8)", r"(5\;6\;9)", r"\in S_9",
-        color=BLACK, font_size=48
+        color=BLACK, font_size=42
     ).next_to(judul, DOWN, buff=0.5)
     
-    sigma_tex[1].set_color(BLUE_E)     # (1 3 4)
-    sigma_tex[2].set_color(RED_E)      # (2 7 8)
-    sigma_tex[3].set_color(GREEN_E)    # (5 6 9)
+    # sigma_tex[1].set_color(BLUE_E)     # (1 3 4)
+    # sigma_tex[2].set_color(RED_E)      # (2 7 8)
+    # sigma_tex[3].set_color(GREEN_E)    # (5 6 9)
 
     subst_final = MathTex(
         r"C_{S_9}(\sigma) \cong \mathbb{Z}_3^3 \rtimes S_3",
-        color=BLACK, font_size=36
+        color=BLACK, font_size=42
     ).next_to(sigma_tex, DOWN, buff=0.8)
 
     s.play(
@@ -64,8 +64,8 @@ def centralizer(s):
     s.wait(2)
 
     # 4. Pisahkan C_S_9 dan Z_3^3 x S_3 dengan warna
-    grup_kiri = MathTex(r"C_{S_9}(\sigma)", color=BLUE_E, font_size=40).move_to(LEFT * 3.5 + UP * 1.0)
-    grup_kanan = MathTex(r"\mathbb{Z}_3^3 \rtimes S_3", color=RED_E, font_size=40).move_to(RIGHT * 3.5 + UP * 1.0)
+    grup_kiri = MathTex(r"C_{S_9}(\sigma)", color=BLUE_E, font_size=40).move_to(LEFT * 3.5 + UP * 0.8)
+    grup_kanan = MathTex(r"\mathbb{Z}_3^3 \rtimes S_3", color=RED_E, font_size=40).move_to(RIGHT * 3.5 + UP * 0.8)
 
     s.play(
         FadeOut(teorema), 
@@ -75,8 +75,8 @@ def centralizer(s):
     )
 
     # Panah melengkung
-    panah_atas = CurvedArrow(grup_kiri.get_top() + RIGHT*0.2 + UP*0.2, grup_kanan.get_top() + LEFT*0.2 + UP*0.2, angle=-TAU/8, color=BLACK)
-    panah_bawah = CurvedArrow(grup_kanan.get_bottom() + LEFT*0.2 + DOWN*0.2, grup_kiri.get_bottom() + RIGHT*0.2 + DOWN*0.2, angle=-TAU/8, color=BLACK)
+    panah_atas = CurvedArrow(grup_kiri.get_right() + RIGHT*0.2 + UP*0.2, grup_kanan.get_left() + LEFT*0.2 + UP*0.2, angle=-TAU/12, color=GRAY)
+    panah_bawah = CurvedArrow(grup_kanan.get_left() + LEFT*0.2 + DOWN*0.2, grup_kiri.get_right() + RIGHT*0.2 + DOWN*0.2, angle=-TAU/12, color=GRAY)
 
     label_phi = MathTex(r"\Phi", color=BLACK, font_size=32).next_to(panah_atas, UP, buff=0.1)
     label_phi_inv = MathTex(r"\Phi^{-1}", color=BLACK, font_size=32).next_to(panah_bawah, DOWN, buff=0.1)
@@ -95,7 +95,8 @@ def centralizer(s):
         (r"(1\;3\;4)(5\;6\;9)", r"((1,0,1), id)"),
         (r"(1\;2)(3\;7)(4\;8)", r"((0,0,0), (1\;2))"),
         (r"(1\;5\;3\;6\;4\;9)(2\;8\;7)", r"((1,2,0), (1\;3))"),
-        (r"(1\;4\;3)(2\;9\;7\;5\;8\;6)", r"((2,2,2), (2\;3))")
+        (r"(1\;4\;3)(2\;9\;7\;5\;8\;6)", r"((2,2,2), (2\;3))"),
+        (r"\vdots", r"\vdots"),
     ]
 
     kiri_y_start = grup_kiri.get_bottom()[1] - 0.5
@@ -119,6 +120,7 @@ def centralizer(s):
         kiri_group.add(center_kiri)
         kanan_group.add(center_kanan)
         s.wait(0.3)
+    
 
     # 6. Kotakin kedua himpunan
     kotak_kiri = SurroundingRectangle(VGroup(grup_kiri, kiri_group), color=BLUE_E, buff=0.3)
